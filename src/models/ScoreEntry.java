49aeb6604c09e333e0912e5dba42437af95cf207
Patrick <patrick@barco.dev>
2026-08-24 10:20:39 +0200

    save and load from json

---
+++ src/models/ScoreEntry.java
@@ -0,0 +1,20 @@
+package models;
+
+public class ScoreEntry {
+
+    private String name;
+    private int score;
+
+    public ScoreEntry(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+}
